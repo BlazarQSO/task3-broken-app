b5226a42ffd65957f2d9a751fa165985beab6e5d
BlazarQSO <BlazarQSO@gmail.com>
2021-05-22 01:53:54 +0300

    feat: add env variables

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres'
 })
